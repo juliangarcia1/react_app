e44b40dc964ae6bb3284668c9a4999de82d0bf85
Julian <juliancatorce@gmail.com>
2018-05-02 18:35:40 -0500

    Added project bulletin-board created

---
+++ bulletin-board/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Note from './Note';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(
+  <Note />,
+  document.getElementById('root')
+);
+// registerServiceWorker()
