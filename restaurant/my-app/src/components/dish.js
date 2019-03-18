5e5d3590b0b9bd7b03059bcc226be6e9cdb31a2a
Julian Garcia <juliancatorce@gmail.com>
2019-03-17 19:45:48 -0600

    Update:dish component, a fuction returns the lenght of ingredient array

--- restaurant/my-app/src/components/dish.js
+++ restaurant/my-app/src/components/dish.js
@@ -24,15 +24,27 @@ export class Ingredient extends Component {
 }
 
 class Dish extends Component {
+    ingredients = ["Tortilla", "Carne", "Cebolla"];
+    ingredientes = {uno:"1", dos:"2"};
+    countIngredients() {
+        return this.ingredients.length;
+    }
+
     render() {
         return (
             <div className="dish">
                 <h1> {this.props.name} </h1>
-                {this.props.quantity}
-                < Ingredient />
-                <Button variant="contained" color="primary">
-                Send
-                </Button>
+                <h3>Total ingredients</h3>
+                <ul>
+                    {this.ingredients.map(ingredient =>(
+                        <li>{ingredient}</li>
+                    ))}
+                    {this.ingredientes.map(([key, value]) => (
+                            <li>{key}</li>
+                    ))}          
+                </ul>
+                <h3>{this.countIngredients()}</h3>
+                <Button variant="contained" color="primary"> Send </Button>
             </div>
         )
     }
