05cb31e6955b249f3a1c2f02a16da8da516b9ca0
Julian Garcia <juliancatorce@gmail.com>
2019-03-18 12:25:00 -0600

    Update: app.js and dished.js, material's button to go from app to dishes and go back to app route

--- restaurant/my-app/src/App.js
+++ restaurant/my-app/src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button'
 
 import Header from './components/header';
 import Dish from './components/dish';
+import NewDish from './components/newDish';
 
 import './styles/App.css';
-import NewDish from './components/newDish';
 // import  './styles/dish.css'; //This should be here because if in one component ti is import to all app anyway
 
 
 class App extends Component {
   dish = "tacos";
   dishes =["Tacos", "Ceviche", "Paella"] 
+  showDishes = e => {
+    e.preventDefault();
+    this.props.history.push("/dishes")
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <NewDish/>
-        <Dish name={this.dish} quantity="3"/>
-        Yo como {this.dish}
+        <Button variant="contained"
+        color="secondary" onClick={this.showDishes}>
+        Elegir
+        </Button>
+        {/* <Dish name={this.dish} quantity="3"/>
+        Yo como {this.dish} */}
         <ul>
           {
             this.dishes.map((dish, index)=>(
--- restaurant/my-app/src/components/dishes.js
+++ restaurant/my-app/src/components/dishes.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 
 export class Dishes extends Component {
+    goBack = e => {
+        this.props.history.push("/")
+    }
     render() {
         return (
             <div>
                 <h1>Dishes</h1>
+                <Button variant="contained"
+                color="primaray"
+                onClick={this.goBack}>
+                    Go Back
+                </Button>
             </div>
         );
     }
