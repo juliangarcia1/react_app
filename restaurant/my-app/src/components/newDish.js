386979aa20bd13b1091f67fe8f93560ef95a7cde
Julian Garcia <juliancatorce@gmail.com>
2019-03-17 20:48:04 -0600

    Added:newDish.js component file with form to add a new dish

---
+++ restaurant/my-app/src/components/newDish.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Fab, TextField } from '@material-ui/core';
+
+
+import AddIcon from '@material-ui/icons/Add';
+
+class NewDish extends Component {
+    addDish(event) {
+        console.log('Add dish');
+        event.preventDefault(); // Avoid reload the webpage
+    }
+    render(){
+        console.log(this);
+        return(
+            <form autoComplete="off" onSubmit={this.addDish}>
+                <TextField 
+                    label= "Platillo..."
+                    type="text"
+                    margin="normal"
+                    variant="outlined"
+                />
+                <Fab color="primary" size="medium"
+                    className="dish-form-icon" onClick={this.addDish}>
+                    <AddIcon/>
+                </Fab>
+            </form>
+        );
+    }
+}
+
+export default NewDish;
