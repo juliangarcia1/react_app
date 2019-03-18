d41dd96535170bb6002e5d1a4dea3d4076c7aa45
Julian Garcia <juliancatorce@gmail.com>
2019-03-17 23:55:54 -0600

    Update: newDish.js, with constructor to try how binding method is useful in constructor method

--- restaurant/my-app/src/components/newDish.js
+++ restaurant/my-app/src/components/newDish.js
@@ -5,12 +5,18 @@ import { Fab, TextField } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 class NewDish extends Component {
+
+    constructor() {
+        super();
+        this.addDish = this.addDish.bind(this);
+    }
     addDish(event) {
         console.log('Add dish');
-        event.preventDefault(); // Avoid reload the webpage
+        event.preventDefault(); // Avoid reload the webpage and clean text fields, etc.
+        console.log(this)
     }
     render(){
-        console.log(this);
+        // console.log(this);
         return(
             <form autoComplete="off" onSubmit={this.addDish}>
                 <TextField 
