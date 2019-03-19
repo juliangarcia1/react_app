6c6e3d8f44e6736bf7570aaf47e7af69a2184890
Julian Garcia <jg.garcia@globant.com>
2019-03-19 17:07:49 -0600

    Update:dishes.js, unused code removed

--- restaurant/my-app/src/components/dishes.js
+++ restaurant/my-app/src/components/dishes.js
@@ -1,19 +1,7 @@
 import React, {Component} from 'react';
-import {
-    Button,
-    List, 
-    ListItem,
-    ListSubheader, 
-    ListItemText, 
-    ListItemIcon,
-    GridList,
-    GridListTile
+import {GridList} from '@material-ui/core';
 
-} from '@material-ui/core';
-import Restaurant from '@material-ui/icons/Restaurant'
 import Dish from './dish';
-
-import data from '../assets/data/dishes.json'
 
 export class Dishes extends Component {
     goBack = e => {
@@ -22,37 +10,6 @@ export class Dishes extends Component {
     render() {
         return (
             <div>
-                <h1>Dishes</h1>
-                <Button variant="contained"
-                color="secondary"
-                onClick={this.goBack}>
-                    Go Back
-
-                </Button>
-                {/* {data.dishes.map(((dish, index) => (
-                    <div key={index}>
-                        <div>{dish.name}</div>
-                        <div>{dish.ingredients}</div>
-                        <br/>
-                    </div>
-                )))
-                } */}
-                {/* <List
-                    component="nav"
-                    subheader={ <ListSubheader
-                                    component="div">Dishes</ListSubheader>
-                              }
-                    >
-                {data.dishes.map((dish, index) => (
-                    <ListItem button key={index}>
-                        <ListItemIcon>
-                         <Restaurant />
-                        </ListItemIcon>
-                        <ListItemText inset primary={dish.name} /> 
-                    </ListItem>
-                    ))
-                }
-                </List> */}
                 <GridList>
                    {data.dishes.map((dish, index) => (
                         <Dish key={index} name={dish.name}
