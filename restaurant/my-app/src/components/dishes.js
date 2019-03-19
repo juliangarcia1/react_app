5aaada9335572c2e0aa184e71b475fcdbff19fb2
Julian Garcia <juliancatorce@gmail.com>
2019-03-18 19:42:54 -0600

    Update:dishes.js, displaying dishes.json content

--- restaurant/my-app/src/components/dishes.js
+++ restaurant/my-app/src/components/dishes.js
@@ -1,5 +1,17 @@
 import React, {Component} from 'react';
-import Button from '@material-ui/core/Button';
+import {
+    Button,
+    List, 
+    ListItem,
+    ListSubheader, 
+    ListItemText, 
+    ListItemIcon
+
+} from '@material-ui/core';
+import Restaurant from '@material-ui/icons/Restaurant'
+
+
+import data from '../assets/data/dishes.json'
 
 export class Dishes extends Component {
     goBack = e => {
@@ -10,10 +22,35 @@ export class Dishes extends Component {
             <div>
                 <h1>Dishes</h1>
                 <Button variant="contained"
-                color="primaray"
+                color="secondary"
                 onClick={this.goBack}>
                     Go Back
+
                 </Button>
+                {/* {data.dishes.map(((dish, index) => (
+                    <div key={index}>
+                        <div>{dish.name}</div>
+                        <div>{dish.ingredients}</div>
+                        <br/>
+                    </div>
+                )))
+                } */}
+                <List
+                    component="nav"
+                    subheader={ <ListSubheader
+                                    component="div">Dishes</ListSubheader>
+                              }
+                    >
+                {data.dishes.map((dish, index) => (
+                    <ListItem button key={index}>
+                        <ListItemIcon>
+                         <Restaurant />
+                        </ListItemIcon>
+                        <ListItemText inset primary={dish.name} /> 
+                    </ListItem>
+                    ))
+                }
+                </List>
             </div>
         );
     }
